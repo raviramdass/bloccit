598748b957a767936f6b2f42e0e8d311d5036714
Ravi Ramdass <ravi.ramdass@gmail.com>
2014-12-13 20:31:20 +0000

    Updated bloccit layout files 12-13-14 @ 3:31

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,10 +6,32 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
-
+ 
   def new
-  end
-
+    @post = Post.new
+  end  
+  
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :body))
+    if @post.save
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post.  Please try again."
+      render :new
+    end
+   end
+  
   def edit
-  end
+    @post = Post.find(params[:id])
+  end  
+    
+  def update
+    @post = Post.find(params[:id])
+    if    @post.update_attributes(params.require(:post).permit(:title, :body))
+      flash[:error] = "There was an error saving the post.  Please try again."
+      render :edit
+    end
+  end 
+      
 end
