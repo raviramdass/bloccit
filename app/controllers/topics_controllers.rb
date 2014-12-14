b160eafdef787c1bb0fca04122a8f248dce7a3bb
Ravi Ramdass <ravi.ramdass@gmail.com>
2014-12-14 03:47:55 +0000

    Update with pagination

---
+++ app/controllers/topics_controllers.rb
@@ -0,0 +1,9 @@
+def index
+    @topics = Topic.paginate(page: params[:page], per_page: 10)
+    authorize @topics
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+     @posts = @topic.posts.paginate(page: params[:page], per_page: 10)
+  end
---
+++ db/migrate/[timestamp]_devise_create_users.rb
@@ -0,0 +1,12 @@
+class DeviseCreateUsers < ActiveRecord::Migration
+  def change
+    create_table(:users) do |t|
+       ## Customization
+       t.string :name
+       t.string   :confirmation_token
+       t.datetime :confirmed_at
+       t.datetime :confirmation_sent_at
+       t.string   :unconfirmed_email # Only if using reconfirmable
+    end
+  end
+end
