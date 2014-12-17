4d6637565744ae3ccbd3e55dfd304ded09633540
Ravi Ramdass <ravi.ramdass@gmail.com>
2014-12-17 15:27:03 +0000

    Updated user database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141217015947) do
+ActiveRecord::Schema.define(version: 20141217122728) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -43,8 +43,13 @@ ActiveRecord::Schema.define(version: 20141217015947) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
