214d6c651fd47b2dabd1d96c102509a64c6d3ade
Ravi Ramdass <ravi.ramdass@gmail.com>
2014-12-18 16:33:05 +0000

    moved config call under module

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,6 @@ require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
-config.assets.enabled = true
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -27,5 +26,6 @@ module Bloccit
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.enabled = true
   end
 end
