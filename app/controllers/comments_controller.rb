c1f64b393de14ac8c41ef7dc23642f6fbbbd1e78
Ravi Ramdass <ravi.ramdass@gmail.com>
2014-12-22 22:36:52 +0000

    Updated comments functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,32 +1,32 @@
 class CommentsController < ApplicationController
   def show
-    @comments = Comments.find(params[:user])
+    @comment = Comment.find(params[:user])
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
   end
   
   def new
-    @comments = Comments.find(params[:user])
+    @comment = Comment.find(params[:user])
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
-    authorize @comments
+    authorize @comment
   end
   
   def create
-    @comments = Comments.new(comments_param)
-    @comments.save
-    redirect_to @comments
+    @post = Post.find(params[:id])
+    @comment = @post.comment.create(comment_params)
+    redirect_to post_path(@comment)
   end
   
   def edit
-    @comments = Comments.find(params[:user])
+    @comment = Comment.find(params[:user])
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
-    authorize @comments
+    authorize @comment
   end
   
 private
-  def comments_params
-    (params.require(:user).permit(:comment, :text)
+  def comment_params
+    params.require(:comment).permit(:user, :body)
   end
 end
